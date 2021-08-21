1f9c5abeb215abd72064c1424108fd6943f73988
santanu1k95 <santa1k95@outlook.com>
2021-08-21 06:16:10 +0530

    Init Commit

---
+++ src/main/java/com/example/demo/SpGtFindingFalconApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpGtFindingFalconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpGtFindingFalconApplication.class, args);
+	}
+
+}
